482a55764a5e587bf999aa8afe0429c3c0e02d18
Jayesh Vekariya <iamjv8@gmail.com>
2024-02-19 08:00:40 +0000

    Changes made in Module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { InterceptorService } from './services/interceptor.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthMappingService } from './mapping/services/auth-mapping.service';
+import { ProductsMappingService } from './mapping/services/products-mapping.service';
+import { CartMappingService } from './mapping/services/cart-mapping.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +23,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     provide: HTTP_INTERCEPTORS,
     useClass: InterceptorService,
     multi: true,
-  }],
+  },AuthMappingService, ProductsMappingService, CartMappingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
